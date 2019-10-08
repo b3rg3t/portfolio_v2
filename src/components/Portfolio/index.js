7bc5f18e07f62ce060f3188c69576d5349ba8bc8
b3rg3t <berget_90db@hotmail.com>
2019-10-08 22:01:06 +0200

    update on ham-nav and portfolio with description + links

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../Portfolio/portfolio.scss";
 import ImageGallery from "react-image-gallery";
-// import * as Icon from "react-feather";
+import * as Icon from "react-feather";
 
 import { projects } from "./projects";
 
@@ -10,25 +10,56 @@ const Portfolio = () => {
   const SetState = index => {
     setCurrentIndex(index);
   };
-  // var projectSpread = { ...projects };
-  // var res = Object.keys(projectSpread).map(k => {
-  //   return [projectSpread[k]];
-  // });
-
-  // console.log(res);
-  const showDescripion = () => {
+  const ShowDescripion = () => {
     console.log("showDesciption ran");
-    var content = projects.map(proj => {
-      if (proj.id === currentIndex) {
-        return proj.description + " " + proj.github;
-      } else {
-      }
+    var project = projects.filter(project => {
+      return project.id === currentIndex ? project : null;
     });
-    var desContainer = document.querySelector(".portfolio__description");
-    return (desContainer.innerHTML = content);
+    return (
+      <div className="showDescription">
+        {project
+          ? project.map((proj, index) => (
+              <div key={index} className="showDescription__container">
+                <h4>{proj.originalAlt}</h4>
+                <p>{proj.text}</p>
+                <div className="showDescription__container__links">
+                  {proj.github ? (
+                    <div>
+                      <span>Github: </span>
+                      <a
+                        className="link"
+                        href={proj.github}
+                        title="Github repo"
+                      >
+                        <Icon.GitHub className="navigation__container__icons" />
+                      </a>
+                    </div>
+                  ) : (
+                    <div>
+                      <span>Github: </span> <Icon.X />
+                    </div>
+                  )}
+                  {proj.website ? (
+                    <div>
+                      <span>Website: </span>
+                      <a
+                        className="link"
+                        href={proj.website}
+                        title={proj.originalAlt}
+                      >
+                        <Icon.ExternalLink className="navigation__container__icons" />
+                      </a>
+                    </div>
+                  ) : null}
+                </div>
+              </div>
+            ))
+          : null}
+      </div>
+    );
   };
   useEffect(() => {
-    showDescripion();
+    ShowDescripion();
   });
   return (
     <div className="portfolio">
@@ -36,20 +67,21 @@ const Portfolio = () => {
         <h3 className="portfolio__container__head">Portfolio</h3>
       </div>
       <div className="portfolio__imageGallery">
-        <ImageGallery
-          items={projects}
-          thumbnailPosition={"right"}
-          lazyLoad={true}
-          showPlayButton={false}
-          showFullscreenButton={false}
-          onSlide={index => SetState(index)}
-          onThumbnailClick={(e, index) => SetState(index)}
-        />
+        <div className="portfolio__imageGallery__pictures">
+          <ImageGallery
+            items={projects}
+            thumbnailPosition={"right"}
+            lazyLoad={true}
+            showPlayButton={false}
+            showFullscreenButton={false}
+            onSlide={index => SetState(index)}
+            onThumbnailClick={(e, index) => SetState(index)}
+          />
+        </div>
+        <div className="portfolio__imageGallery__description">
+          <ShowDescripion />
+        </div>
       </div>
-      <div
-        className="portfolio__description"
-        onClick={e => console.log(e.target)}
-      ></div>
     </div>
   );
 };
--- src/components/Portfolio/projects.js
+++ src/components/Portfolio/projects.js
@@ -4,47 +4,49 @@ import DavidBerg from "../../images/AndersBerg.png";
 // import { myRenderItem } from "./index"
 
 const projects = [
+  // {
+  //   id: 0,
+  //   original: ProjectXY,
+  //   thumbnail: ProjectXY,
+  //   originalAlt: "Grex",
+  //   thumbnailLabel: "Project XY",
+  //   thumbnailTitle: "Project XY",
+  //   description: "Project XY",
+  //   sizes: "height: 300px, width: 300px",
+  //   github: "https://github.com/b3rg3t/grex_candy"
+  // },
   {
     id: 0,
     original: ProjectXY,
     thumbnail: ProjectXY,
-    originalAlt: "Grex",
+    originalAlt: "Project XY",
+    // description: "Project XY",
     thumbnailLabel: "Project XY",
     thumbnailTitle: "Project XY",
-    description: "Project XY",
-    sizes: "height: 300px, width: 300px",
-    github: "https://github.com/b3rg3t/grex_candy"
+    // github: "https://github.com/b3rg3t/leo_quire",
+    website: "https://github.com/b3rg3t/leo_quire",
+    text: "This a website for a swedish rockband that needs a new responsive website that works properly on mobile phones and tablets."
   },
   {
     id: 1,
-    original: ProjectXY,
-    thumbnail: ProjectXY,
-    originalAlt: "Project XY",
-    description: "Project XY",
-    thumbnailLabel: "Project XY",
-    thumbnailTitle: "Project XY",
-  },
-  {
-    id: 2,
     original: Quire,
     thumbnail: Quire,
     originalAlt: "Quire notepad",
     thumbnailLabel: "Quire",
-    description: "Quire",
-    github: "https://github.com/b3rg3t/leo_quire"
+    originalTitle: "Quire",
+    github: "https://github.com/b3rg3t/leo_quire",
+    website: "https://github.com/b3rg3t/leo_quire"
   },
   {
-    id: 3,
+    id: 2,
     original: DavidBerg,
     thumbnail: DavidBerg,
     originalAlt: "David Anders",
     thumbnailLabel: "David Berg",
     originalTitle: "David Anders",
-    description: "David Anders",
-    github: "https://github.com/b3rg3t/leo_quire"
+    github: "https://github.com/b3rg3t/leo_quire",
+    website: "https://github.com/b3rg3t/leo_quire"
   }
 ];
-
-// https://github.com/b3rg3t/leo_quire
 
 export { projects };
